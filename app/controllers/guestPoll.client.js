41714149e3f68930135051b1e47702be375cc681
Nnenanya Obinna Kingsley <obinnaeye@yahoo.com>
2017-02-20 21:20:06 +0000

    tidy guest user

--- app/controllers/guestPoll.client.js
+++ app/controllers/guestPoll.client.js
@@ -107,14 +107,10 @@ function loadPolls (result){
       len--;
     }
     
-    //Add your own options if you desire
-    optionHtml += '<option value="new">Add Your Own Option</option>';
-    
     document.getElementById("pollViewDesc").innerHTML = firstPoll.description;
     document.getElementById("pollViewHead").innerHTML = firstPoll.name + ':<span> by ' + firstPoll.author + '</span>';
     document.getElementById("chartTitle").innerHTML = firstPoll.name + ': Pie Chart.';
     document.getElementById("pollSelection").innerHTML = optionHtml;
-    document.getElementById("pollSelection").addEventListener('change', addOptionDisplay, false);
     document.getElementById("pollLink").addEventListener("click", showLink, false);
     document.getElementById("linkBoxClose").addEventListener("click", closeLink, false);
     
@@ -123,6 +119,20 @@ function loadPolls (result){
   
   conditionalDraw(voteCounter, data, options);
   
+  }
+  
+  //show link box
+  function showLink(){
+      document.getElementById("mask").style.display = "block";
+      document.getElementById("linkBox").style.display = "block";
+      var pollID = document.getElementsByClassName("poll-ref active")[0].getAttribute("id");
+      document.getElementById("linkBoxLink").innerHTML = appUrl + "/username/polls/" + pollID;
+  }
+  
+  //close link box
+  function closeLink(){
+      document.getElementById("mask").style.display = "none";
+      document.getElementById("linkBox").style.display = "none";
   }
 
 function updateChart(obj){
@@ -213,61 +223,7 @@ submitVote.addEventListener('click', sendVote, false);
 
 ajaxFunctions.ready(ajaxFunctions.ajaxRequest('GET', appUrl + "/api/polls-array", loadPolls));
 
-//Add your own option function
-function addOptionDisplay(){
-  var value = document.getElementById("pollSelection").value;  
-  if(value === "new"){
-    document.getElementById("newOption").style.display = "block";
-  }
-}
 
-//Cancel new option window;
-function cancelOption(){
-  document.getElementById("newOption").style.display = "none";
-  document.getElementById("pollSelection").value = "";
-  document.getElementById("newOptionValue").value = "";
-  document.getElementById("optionWarning").innerHTML = "";
-}
-document.getElementById("newOptionCancel").addEventListener('click', cancelOption, false);
-
-//Add new option
-function newOption(){
-  status = "new option";
-  var newOption = document.getElementById("newOptionValue").value;  
-  var poll = document.getElementById(pollID).getAttribute("poll"),
-      options = JSON.parse(poll).options;
-  var filtered = options.filter(function(value){return value.name === newOption});
-  
-  if(filtered.length >= 1){
-    document.getElementById("optionWarning").innerHTML = "This option already exists. Cancel or enter a new option!";
-  }else if(newOption === ""){
-    document.getElementById("optionWarning").innerHTML = "You have not entered any option!";
-  }
-  else{
-    document.getElementById("newOption").style.display = "none";
-    ajaxFunctions.ajaxRequest('POST', appUrl + "/api/newoption?pollid=" + pollID + "&option=" + newOption, updateChart);    
-    //document.getElementById("pollSelection").value = "";
-    document.getElementById("newOptionValue").value = "";
-    document.getElementById("optionWarning").innerHTML = "";
-  }  
-}
-document.getElementById("newOptionOk").addEventListener('click', newOption, false);
-
-//Create New Poll and Add the poll
-var newpollButton = document.getElementById("createPoll");
-function createPoll(){
-  document.getElementById("newPoll").style.display = "block"
-}
-newpollButton.addEventListener('click', createPoll, false);
-
-function cancelCreatePoll(){
-    document.getElementById("newPoll").style.display = "none";
-    document.getElementById("createInputName").value = "";
-    document.getElementById("createInputDesc").value = "";
-    document.getElementById("createInputOptions").value = "";
-    document.getElementById("createPollWarning").innerHTML ="";
-}
-document.getElementById("createPollCancel").addEventListener('click', cancelCreatePoll, false);
 
 //Reset globalPoll variable
 function updatePoll(data){
@@ -284,59 +240,6 @@ function updatePoll(data){
   addEventToClass (pollClick)
 }
 
-//add new poll to database
-function addPoll(){
-  var name = document.getElementById("createInputName").value;
-  var title = name.slice(0, 1).toUpperCase() + name.slice(1);
-  var desc = document.getElementById("createInputDesc").value;
-  var options = document.getElementById("createInputOptions").value;
-      
-  var warning = document.getElementById("createPollWarning");
-  
-  var filtered = globalPoll.filter(function(value){return value.name.toLowerCase() === title.toLowerCase()});
-  if (!title.match(/\w/g) || !desc.match(/\w/g) || !options.match(/\w/g)){
-    warning.innerHTML = "Please Fill in All the Info With Valid Characters!";
-  }else if(filtered.length >= 1){
-    warning.innerHTML = "Poll Already exists with the Title. Please choose a different Title!";
-  }else{
-    var optionsArr1 = options.split(/,\W*/g),
-      optionsArr = [];
-  //convert options to array of uppercase first letter
-  optionsArr1.forEach(function(x){
-    var upperCase = x.slice(0, 1).toUpperCase() + x.slice(1).toLowerCase();
-    optionsArr.push(upperCase);
-  });
-  
-  
- var filteredOptions = optionsArr.filter( 
-      function( item, index, inputArray ) {
-        return inputArray.indexOf(item) == index;
-      });
-  options = filteredOptions.join(",");
-  desc = desc.slice(0, 1).toUpperCase() + desc.slice(1).toLowerCase();
-    ajaxFunctions.ajaxRequest('POST', appUrl + "/api/polls-array?options=" + options + "&desc=" + desc+ "&name=" + title, updatePoll);
-    
-    //close create poll window
-    document.getElementById("newPoll").style.display = "none";
-    document.getElementById("createInputName").value = "";
-    document.getElementById("createInputDesc").value = "";
-    document.getElementById("createInputOptions").value = "";
-    warning.innerHTML ="";
-  } 
-}
-document.getElementById("createPollOk").addEventListener('click', addPoll, false);
-
-//show link box
-  function showLink(){
-      document.getElementById("mask").style.display = "block";
-      document.getElementById("linkBox").style.display = "block";
-  }
-  
-  //close link box
-  function closeLink(){
-      document.getElementById("mask").style.display = "none";
-      document.getElementById("linkBox").style.display = "none";
-  }
 
 })();
 
